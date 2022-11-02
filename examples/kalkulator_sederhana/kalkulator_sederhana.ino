10d13db4f8340b5427a6390135011c0e936bdb32
adibw99 <warenxadibjavalans@gmail.com>
2022-11-02 18:14:00 +0700

    Update kalkulator_sederhana.ino

--- examples/kalkulator_sederhana/kalkulator_sederhana.ino
+++ examples/kalkulator_sederhana/kalkulator_sederhana.ino
@@ -10,21 +10,25 @@ void setup() {
 }
 
 void loop() {
-  Serial.println("Pilih Menu = ");
+  Serial.print("Pilih Menu = ");
 
   while (Serial.available() == 0) {
   }
 
   int pilihanMenu = Serial.parseInt();
+  Serial.println(pilihanMenu);
 
   switch (pilihanMenu) {
     case 1:
       // penjumlahan
+      delay(3000);
       Serial.print("Masukkan Angka = ");
       int a = Serial.parseInt();
+      Serial.println(a);
       delay(3000);
       Serial.print("Jumlahkan dengan Angka = ");
       int b = Serial.parseInt();
+      Serial.println(b);
       delay(3000);
       int hasilJumlah = Calc.penjumlahan(a,b);
       Serial.print("Hasil Penjumlahan = ");
@@ -44,7 +48,8 @@ void loop() {
       Serial.println(hasilKurang);
       break;
 
-    default:
+    case 3:
       Serial.println("Pilih Menu Valid!!");
+      break;
   }
 }
